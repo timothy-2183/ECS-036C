6b7d48b1100d7f3d2fdee49888b66c95e4e396d6
timothy <timothyhansh@gmail.com>
2025-01-28 21:14:18 +0000

    Cmake and google tssting

---
+++ build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vscode/ECS-036C/googletest/googletest
+# Build directory: /home/vscode/ECS-036C/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
